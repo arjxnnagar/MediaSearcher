07eab625711e296a955d2c2a04eb6f19888e0457
Arjun Nagar <arjunnagar012@gmail.com>
2026-07-04 23:39:17 +0530

    Completed Project

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,7 +1,12 @@
 import { configureStore } from "@reduxjs/toolkit";
+import searchReducer from "./features/searchSlice";
+import collectionReducer from "./features/collectionSlice";
 
-export default configureStore({
-    reducer:{
-        
-    }
-})
+export const store = configureStore({
+  reducer: {
+    search: searchReducer,
+    collection: collectionReducer,
+  },
+});
+
+export default store
